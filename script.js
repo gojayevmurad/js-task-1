fad312a52f7634ff60b678ff88fd88ba794782b7
Your Name <you@example.com>
2023-02-01 11:57:17 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+// let numbers = prompt('Numbers')
+// let arr = numbers.split(',')
+// let num = 0;
+// for(let i = 0; i<arr.length; i++){
+
+//     if(+arr[i]>num){
+//         num = +arr[i]
+//     }
+    
+// }
+// console.log(num)
+
+
+
+
+
+
+
+
+
+
+
+
+// Input ile daxil olunan ədədin sade bolenlerini cixartsin
+
+// let num = +prompt('Number:');
+
+// for (let i = 2; i <= num; i++) {
+//   let counter = 0;
+//   if (num % i == 0) {
+//     for (let a = 1; a <= i; a++) {
+//       if (i % a == 0) {
+//         counter += 1;
+//       }
+//     }
+
+//     if (counter == 2) {
+//       console.log(i);
+//     }
+//   }
+// }
+
+// *** 3 reqemli eded daxil edirik ededin evveline ve sonuna string ile 3 elave edirik
+
+// let num = prompt("Number");
+// if (num.length == 3) {
+//   console.log(`3${num}3`);
+// }
+
+// Input ile daxil olunan ədədin rəqəmlərindən ən böyüyünü tapın.
+
